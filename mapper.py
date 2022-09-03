5aadf35cb2133c3437efd8e1e28140cbb84f9f93
Robelasmare <110356584+robelasmare@users.noreply.github.com>
2022-09-03 12:37:03 -0600

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -6,10 +6,15 @@ for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
 
-   # split the line into words; splits on any whitespace
+    # make lower case
+    line = line.lower()
+
+    # split the line into words; splits on any whitespace
     words = line.split()
 
-    # output tuples (word, 1) in tab-delimited format
-       for word in words:
-           print '%s\t%s' % (word, "1")
+  
+    # notice the loop is different
 
+    for word in words:
+        if word not in stopwords:
+         print '%s\t%s' % (word, "1")
