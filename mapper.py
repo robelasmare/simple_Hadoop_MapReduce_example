236b97b10bd061ba246babce7eb85fb11b492fc5
Robelasmare <110356584+robelasmare@users.noreply.github.com>
2022-09-03 11:29:42 -0600

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -6,18 +6,10 @@ for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
 
-    # make lower case
-    line = line.lower()
-
-    # split the line into words; splits on any whitespace
+   # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
-    stopwords = set(['the', 'and','a','for','I','go','four','three','be','not','to','be','railings','or','not'])
-
-    # notice the loop is different
-
-    for word in words:
-        if word not in stopwords:
-         print '%s\t%s' % (word, "1")
+       for word in words:
+           print '%s\t%s' % (word, "1")
 
